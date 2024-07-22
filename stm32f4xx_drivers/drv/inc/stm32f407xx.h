abebf643a3470a934c572e8ab8cfda3814f67c06
H-Matus <matusevicius.henrikas@gmail.com>
2024-07-22 19:58:12 +0100

    feat(gpio): Created GPIO register definition.

--- stm32f4xx_drivers/drv/inc/stm32f407xx.h
+++ stm32f4xx_drivers/drv/inc/stm32f407xx.h
@@ -24,10 +24,13 @@
 #define AHB1_BASEADDR	( APB2_BASEADDR + APB2_LEN )
 #define AHB2_BASEADDR	0x50000000
 
+/* Peripheral Specific Offsets */
+#define GPIO_ADDR_OFFSET	0x400
+#define SPI1_ADDR_OFFSET	0x3000
+
 /* Peripheral base addresses: */
 /* GPIO Peripheral Base addresses */
 #define GPIOA_BASEADDR	AHB1_BASEADDR
-#define GPIO_ADDR_OFFSET	0x400
 #define GPIOB_BASEADDR	( GPIOA_BASEADDR + GPIO_ADDR_OFFSET )
 #define GPIOC_BASEADDR	( GPIOB_BASEADDR + GPIO_ADDR_OFFSET )
 #define GPIOD_BASEADDR	( GPIOC_BASEADDR + GPIO_ADDR_OFFSET )
@@ -40,13 +43,32 @@
 #define GPIOK_BASEADDR	( GPIOJ_BASEADDR + GPIO_ADDR_OFFSET )
 
 /* SPI Peripheral Base addresses */
-
+#define SPI1_BASEADDR	( APB2_BASEADDR + SPI1_ADDR_OFFSET )
 
 /* TIM Peripheral Base addresses */
-
+#define TIM1_BASEADDR	APB2_BASEADDR
 
 /* Peripheral Register Structure Definitions */
+/* GPIO */
+typedef struct
+{
+	uint32_t MODER;
+	uint32_t OTYPER;
+	uint32_t OSPEEDR;
+	uint32_t PUPDR;
+	uint32_t IDR;
+	uint32_t ODR;
+	uint32_t BSRR;
+	uint32_t LCKR;
+	uint32_t AFRL;
+	uint32_t AFRH;
+}GPIO_RegDef_t;
 
+/* SPI */
+typedef struct
+{
+
+}SPI_RegDef_t;
 
 /* Peripheral Clock Enable and Disable macros (RCC peripheral) */
 
@@ -64,6 +86,9 @@
 #define GPIOI			( (GPIO_RegDef_t *)GPIOI_BASEADDR )
 #define GPIOJ			( (GPIO_RegDef_t *)GPIOJ_BASEADDR )
 #define GPIOK			( (GPIO_RegDef_t *)GPIOK_BASEADDR )
+
+/* SPI */
+#define SPI1			((SPI_RegDef_t *)SPI1_BASEADDR )
 
 
 #endif /* INC_STM32F407XX_H_ */
